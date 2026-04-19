5df6d96de0b4d7e88037872b60dcad4aecffdc0e
hardik11june2005 <hardikmahajam@gmail.com>
2026-04-19 22:33:42 +0530

    add setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,21 @@ import imageRoutes from "./routes/imageRoutes.js"
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }))
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+  "http://localhost:3000"
+].filter(Boolean); // removes undefined
+console.log( process.env.FRONTEND_URL)
+app.use(cors({ 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}))
 app.use(express.json())
 app.use("/api", imageRoutes)
 
